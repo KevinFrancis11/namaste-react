a250670e3526cfa3a0c744059f251fb8504133fa
Kevin Francis <kevinfrancis967@gmail.com>
2024-12-26 13:11:26 +0530

    Episode-01

---
+++ app.js
@@ -0,0 +1,30 @@
+
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1></h1>
+ *          <h2></h2>
+ *      </div>
+ * </div>
+ * 
+ * 
+ */
+
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I'am an h1 tag"),
+        React.createElement("h2", {}, "I'am an h2 tag")
+        ]),
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I'am an h1 tag"),
+        React.createElement("h2", {}, "I'am an h2 tag")])]);
+
+
+
+// const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello world from react js.");
+console.log(parent);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
