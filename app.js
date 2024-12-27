1b72cd3a026dc943645eee2de4b6082f0c54c64a
Kevin Francis <kevindefrancis@gmail.com>
2024-12-27 09:09:36 +0530

    episode-02

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-
-/**
+import React from "react";
+import ReactDOM from "react-dom/client";
+/** 
  * <div id="parent">
  *      <div id="child">
  *          <h1></h1>
@@ -12,7 +13,7 @@
 
 const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I'am an h1 tag"),
+        [React.createElement("h1", {}, "This is Namaste react 🚀"),
         React.createElement("h2", {}, "I'am an h2 tag")
         ]),
     React.createElement("div", { id: "child" },
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
 # namste react welcom 
+
+# parcel
+- Dev Build
+- Local server
+- HMR- Hot Module Replacement
+- File watching algorithm which is written in cpp  
+- caching - Faster Builds
+- Image optimization
+- If we production build our application 
+ - It will minification our file
+ - Bundling
+ - Compressing
+- consistent hashing
